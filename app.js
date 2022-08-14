33bfff97f60332a6ff6b6d73d552aed9d750be8a
Martin Siles <silesreche.martin@gmail.com>
2022-08-14 17:02:43 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+require('colors');
+
+const { inquirerMenu } = require('./helpers/inquirer');
+const { pausa } = require('./helpers/mensajes');
+
+console.clear();
+
+
+const main = async () => {
+
+    let opt = '';
+
+    do {
+        opt = await inquirerMenu();
+        if( opt !== '0' ) await pausa();
+    } while (opt !== '0' );
+
+    pausa();
+}
+
+main();
+
+
