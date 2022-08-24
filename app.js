8eaf63cedbcfc1bbcb71b5f15cbda3fa402a7d57
Martin Siles <silesreche.martin@gmail.com>
2022-08-24 13:58:19 +0200

    WebServer Listo

--- app.js
+++ app.js
@@ -1,19 +1,45 @@
 const express = require('express')
+var hbs = require('hbs');
+require('dotenv').config();
+
 const app = express()
+const port = process.env.PORT;
+
+// Handlebars
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
 
 //Servir contenido estático
 app.use( express.static('public') );
 
-app.get('/generic', function (req, res) {
-  res.sendFile(__dirname + '/public/generic.html')
+app.get('/', function (req, res) {
+  res.render('home',{
+    nombre: 'Martín',
+    titulo: 'Curso Node'
+  })
 })
 
-app.get('/elements', function (req, res) {
-  res.sendFile(__dirname + '/public/elements.html')
+app.get('/generic', function (req, res) {
+  res.render('generic',{
+    nombre: 'Martín',
+    titulo: 'Curso Node'
+  })
 })
+
+
+
+app.get('/elements', function (req, res) {
+  res.render('elements',{
+    nombre: 'Martín',
+    titulo: 'Curso Node'
+  })})
 
 app.get('*', function (req, res) {
   res.sendFile(__dirname + '/public/404.html')
 })
 
-app.listen(8080)
+app.listen(port, () => {
+
+  console.log('Listening on port ' + port)
+
+})
